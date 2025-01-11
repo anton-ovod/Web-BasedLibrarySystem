a2430c060ba37614ff9e07257247defdc03f263d
Anton Ovod <antovod36@gmail.com>
2025-01-11 03:06:28 +0100

    Created extension for converting file into array of bytes

---
+++ LibraryManagementSystem/Extensions/FileExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementSystem.Extensions
+{
+    public static class FileExtensions
+    {
+        public static byte[] ToByteArray(this IFormFile file)
+        {
+            using var memoryStream = new MemoryStream();
+            file.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
