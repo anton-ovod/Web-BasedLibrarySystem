c0bce1fb168dec11cc8a14741dd53edb4c07438f
Anton Ovod <antovod36@gmail.com>
2025-01-11 18:16:43 +0100

    Added new cookie policy for better safety and enbled HSTS.

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -2,6 +2,8 @@ using LibraryManagementSystem.Contexts;
 using LibraryManagementSystem.Repositories.Implementations;
 using LibraryManagementSystem.Repositories.Interfaces;
 using LibraryManagementSystem.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -21,9 +23,10 @@ namespace LibraryManagementSystem
                 options.UseSqlServer(builder.Configuration.GetConnectionString("SqlDatabaseConnection"));
             });
 
-            builder.Services.AddAuthentication("CookieAuthentication")
-                .AddCookie("CookieAuthentication", options =>
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
                 {
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
                     options.Cookie.HttpOnly = true;
                     options.LoginPath = "/Auth/Login";
                     options.SlidingExpiration = true;
@@ -43,19 +46,27 @@ namespace LibraryManagementSystem
 
             builder.Services.Configure<IISServerOptions>(options =>
             {
-                options.MaxRequestBodySize = 10485760; // 10 MB in bytes
+                options.MaxRequestBodySize = 10485760;
             });
 
             var app = builder.Build();
 
+            var cookiePolicyOptions = new CookiePolicyOptions()
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+                Secure = CookieSecurePolicy.Always,
+                HttpOnly = HttpOnlyPolicy.Always
+
+            };
+
+            app.UseCookiePolicy(cookiePolicyOptions);
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts
             }
-
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
