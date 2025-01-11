24a33494391316f5c01c1379cf95f255b73142e5
Anton Ovod <antovod36@gmail.com>
2025-01-11 18:54:42 +0100

    Created secure password generator.

---
+++ LibraryManagementSystem/Helpers/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace LibraryManagementSystem.Helpers
+{
+    public class PasswordGenerator
+    {
+        public static string GeneratePassword(int length = 8)
+        {
+            const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!.@#$%^&*";
+            
+            var passwordChars = new char[length];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var randomBytes = new byte[length];
+                rng.GetBytes(randomBytes);
+
+                for (int i = 0; i < length; i++)
+                {
+                    passwordChars[i] = allowedChars[randomBytes[i] % allowedChars.Length];
+                }
+            }
+
+            return new string(passwordChars);
+        }
+    }
+}
