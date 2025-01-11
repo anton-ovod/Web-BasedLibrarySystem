8abce6652edebb88ffc82ede5cd244ee8ffa2516
Anton Ovod <antovod36@gmail.com>
2025-01-11 03:05:12 +0100

    Created attribute for file ext validation

---
+++ LibraryManagementSystem/Attributes/FileExtensionValidationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Attributes
+{
+    public class FileExtensionValidationAttribute(string[] allowedExtensions) : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extension = Path.GetExtension(file.FileName)?.ToLower();
+                if (!allowedExtensions.Contains(extension))
+                {
+                    return new ValidationResult($"Please upload a valid image ({string.Join(", ", allowedExtensions)}).");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
