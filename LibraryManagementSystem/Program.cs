5ccaf6ea0f61e9339b27b1c560e3f7ac2a3329da
Anton Ovod <antovod36@gmail.com>
2025-01-12 19:02:45 +0100

    Implemented new events for auth middleware for managing session and auth based on session not only on cookie with user id.

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,11 +1,15 @@
+using System.Runtime.InteropServices.JavaScript;
 using LibraryManagementSystem.Contexts;
+using LibraryManagementSystem.Models;
 using LibraryManagementSystem.Repositories.Implementations;
 using LibraryManagementSystem.Repositories.Interfaces;
 using LibraryManagementSystem.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Security.Claims;
 
 namespace LibraryManagementSystem
 {
@@ -26,12 +30,113 @@ namespace LibraryManagementSystem
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                 {
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
                     options.Cookie.HttpOnly = true;
                     options.LoginPath = "/Auth/Login";
                     options.SlidingExpiration = true;
                     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                     options.Cookie.IsEssential = true;
+
+                    options.Events = new CookieAuthenticationEvents
+                    {
+                        OnValidatePrincipal = async context =>
+                        {
+                            var sessionIdClaim = context.Principal?.FindFirst("SessionId");
+                            if (sessionIdClaim is null)
+                            {
+                                context.RejectPrincipal();
+                                await context.HttpContext.SignOutAsync(
+                                    CookieAuthenticationDefaults.AuthenticationScheme);
+                            }
+
+                            var sessionId = sessionIdClaim?.Value!;
+
+                            var userSessionRepository = context.HttpContext.RequestServices
+                                .GetRequiredService<IUserSessionRepository>();
+                            var session = await userSessionRepository.GetByIdAsync(sessionId);
+
+                            if (session is null)
+                            {
+                                await userSessionRepository.DeactivateAsync(sessionId);
+                                context.RejectPrincipal();
+                                await context.HttpContext.SignOutAsync(
+                                    CookieAuthenticationDefaults.AuthenticationScheme);
+                            }
+                            else if (session!.ExpiresAt <= DateTime.UtcNow)
+                            {
+                                await userSessionRepository.DeactivateAsync(sessionId);
+                                context.RejectPrincipal();
+                                await context.HttpContext.SignOutAsync(
+                                    CookieAuthenticationDefaults.AuthenticationScheme);
+                            }
+                            else
+                            {
+                                var slidingThreshold = session.ExpireTimeSpan / 2;
+
+                                if (DateTime.UtcNow > session.CreatedAt + slidingThreshold)
+                                {
+                                    session.CreatedAt = DateTime.UtcNow;
+                                    await userSessionRepository.UpdateAsync(session);
+                                }
+                            }
+ 
+                        },
+
+                        OnSigningOut = async context =>
+                        {
+                            var principal = context.HttpContext.User;
+
+                            var sessionId = principal?.FindFirst("SessionId")?.Value;
+
+                            if (!string.IsNullOrEmpty(sessionId))
+                            {
+                                var userSessionRepository = context.HttpContext.RequestServices
+                                    .GetRequiredService<IUserSessionRepository>();
+                                await userSessionRepository.DeactivateAsync(sessionId);
+                            }
+                        },
+
+                        OnSigningIn = async context =>
+                        {
+                            var sessionId = Guid.NewGuid().ToString();
+                            var ipAddress = context.HttpContext.Connection.RemoteIpAddress?.ToString() ??
+                                            "Unknown IP address";
+                            var userAgent = context.HttpContext.Request.Headers.UserAgent;
+                            var device = string.IsNullOrEmpty(userAgent.ToString())
+                                ? "Unknown device"
+                                : userAgent.ToString();
+
+                            var userId = context.Principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                            if (userId != null)
+                            {
+                                var userSessionRepository = context.HttpContext.RequestServices
+                                    .GetRequiredService<IUserSessionRepository>();
+
+                                var expirationTimespan = context.Properties.IsPersistent
+                                    ? TimeSpan.FromDays(14)
+                                    : TimeSpan.FromHours(3);
+
+                                var newSession = new UserSession
+                                {
+                                    SessionId = sessionId,
+                                    UserId = int.Parse(userId),
+                                    CreatedAt = DateTime.UtcNow,
+                                    ExpireTimeSpan = expirationTimespan,
+                                    IpAddress = ipAddress,
+                                    Device = device
+                                };
+
+                                await userSessionRepository.AddAsync(newSession);
+                                
+                                if (context.Principal?.Identity is ClaimsIdentity claimsIdentity)
+                                {
+                                    claimsIdentity.AddClaim(new Claim("SessionId", sessionId));
+                                }
+                            }
+
+                        },
+
+                    };
                 });
 
             if(builder.Environment.IsDevelopment())
@@ -43,6 +148,7 @@ namespace LibraryManagementSystem
             builder.Services.AddScoped<EmailService>();
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IBookRepository, BookRepository>();
+            builder.Services.AddScoped<IUserSessionRepository, UserSessionRepository>();
 
             builder.Services.Configure<IISServerOptions>(options =>
             {
